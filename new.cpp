9a09ada43d048675b1708734a58114742573ff16
swikriti <swikritidominators@gmail.com>
2026-06-14 14:46:23 +0530

    update

--- new.cpp
+++ new.cpp
@@ -103,3 +103,24 @@
 // }
 //     return 0;
 // }
+
+
+// #include <iostream>
+// using namespace std;
+// int main(){
+// int count =1;
+// while (count<=n) {
+//     cout<< count<< "" "";
+//     count ++;
+// }
+// cout<< endl;
+//     return 0;
+// }
+
+
+#include <iostream>
+using namespace std;
+int main (){
+
+    return 0;
+}
