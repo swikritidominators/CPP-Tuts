7ca32e1e5efe6645bc443aeb5a7892dda731b823
swikriti <swikritidominators@gmail.com>
2026-06-14 08:06:56 +0530

    update

--- new.cpp
+++ new.cpp
@@ -1,8 +1,37 @@
 
-# include<iostream>
-using namespace std;
-int main () {
-int n=45;
-cout<<(n>=0? "positive" : "negative")<<endl;
-return 0;
-}
+// # include<iostream>
+// using namespace std;
+// int main () {
+// int n=45;
+// cout<<(n>=0? "positive" : "negative")<<endl;
+// return 0;
+// }
+
+// # include<iostream>
+// using namespace std;
+// int main () {
+// int count=1;
+// while (count<=5){
+//     cout<<count<<"";
+//     count ++;
+// }
+// cout<<endl;
+// return 0;
+// }
+
+
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int a,b;
+//     cout<<"Enter a: ";
+//     cin>>a;
+
+//     cout<<"Enter b: ";
+//     cin>>b;
+
+//     int sum= a+b;
+//     cout<<"Enter sum= "<<sum<<endl;
+//     return 0;
+// }
+
